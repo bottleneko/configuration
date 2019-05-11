249cda3c4bb2d5cbe2631cf249244ef54354ed8d
Boris Murashov <bottleneko@gmail.com>
2019-05-12 00:15:01 +0300

    NixOS basic configuration

---
+++ configuration.nix
@@ -0,0 +1,84 @@
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+      ./hardware-configuration.nix
+    ];
+
+  # Use the systemd-boot EFI boot loader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "";
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Select internationalisation properties.
+  # i18n = {
+  #   consoleFont = "Lat2-Terminus16";
+  #   consoleKeyMap = "us";
+  #   defaultLocale = "en_US.UTF-8";
+  # };
+
+  time.timeZone = "Europe/Moscow";
+
+  environment.systemPackages = with pkgs; [
+    emacs xclip zsh tmux docker htop
+  ];
+
+  virtualisation.docker.enable = true;
+
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = { enable = true; enableSSHSupport = true; };
+
+  services.openssh = {
+    enable = true;
+    permitRootLogin = "prohibit-password";
+    passwordAuthentication = false;
+  };
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # Enable CUPS to print documents.
+  # services.printing.enable = true;
+
+  # Enable sound.
+  # sound.enable = true;
+  # hardware.pulseaudio.enable = true;
+
+  services.xserver = {
+    enable = true;
+    xkbOptions = "ctrl:nocaps";
+    layout = "us";
+    windowManager.xmonad = {
+      enable = true;
+      enableContribAndExtras = true;
+      extraPackages = haskellPackages: [
+        haskellPackages.xmonad-contrib
+        haskellPackages.xmonad-extras
+        haskellPackages.xmonad
+      ];
+    };
+    windowManager.default = "xmonad";
+    desktopManager.default = "none";
+    displayManager.slim = {
+      enable = true;
+      defaultUser = "bottleneko";
+    };
+  };
+
+  users.users.bottleneko = {
+    isNormalUser = true;
+    extraGroups = [ "wheel" "docker" ];
+    shell = "/run/current-system/sw/bin/zsh";
+  };
+
+  system.stateVersion = "19.03";
+
+}
